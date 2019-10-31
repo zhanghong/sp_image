d30e4f6268394d573a26170c51b76b00faddc6ef
zhanghongbean <zhanghong_bean@126.com>
2019-10-31 11:19:03 +0800

    delete getContainerSum

--- src/models/Topo.php
+++ src/models/Topo.php
@@ -34,41 +34,6 @@ class Topo extends ActiveRecord
         return $this->hasOne(UserProfile::className(), ['id' => 'user_id']);
     }
 
-    /**
-     * 拓扑配置信息
-     * @Author   zhanghong(Laifuzi)
-     * @DateTime 2019-10-30
-     * @return   array
-     */
-    public function getContainerSum()
-    {
-        $selectFields = [
-            'ROUND(SUM(config_ram)/1024) AS ram_sum',
-            'ROUND(SUM(config_disk)) AS disk_sum',
-            'ROUND(SUM(config_cpu)) AS cpu_sum',
-        ];
-
-        $record = ContainerTopoRelated::find()
-                    ->select($selectFields)
-                    ->where(['topo_id' => $this->id])
-                    ->asArray()
-                    ->one();
-
-        if (empty($record)) {
-            return [
-                'ram_sum' => 0,
-                'disk_sum' => 0,
-                'cpu_sum' => 0,
-            ];
-        }
-
-        foreach ($record as $key => $value) {
-            $record[$key] = intval($value);
-        }
-
-        return $record;
-    }
-
     public function fields()
     {
         $fields = parent::fields();
@@ -82,9 +47,6 @@ class Topo extends ActiveRecord
         return [
             'user',
             'userProfile',
-            'container_sum' => function($model){
-                return $this->containerSum;
-            },
         ];
     }
 }
